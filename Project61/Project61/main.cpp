76594979c6cfda7fb2d1b4dcb88bda77772090ee
Figurka <vanek8898@gmail.com>
2018-12-09 19:36:05 +0300

    sasd

--- Project61/Project61/main.cpp
+++ Project61/Project61/main.cpp
@@ -1,10 +1,21 @@
+#include <iostream> 
 #include <SFML/Graphics.hpp>
-
+#include "map.h"
+using namespace sf;
 int main()
 {
-	sf::RenderWindow window(sf::VideoMode(200, 200), "Lesson 1. kychka-pc.ru");
-	sf::CircleShape shape(100.f);
-	shape.setFillColor(sf::Color::Green);
+	sf::VideoMode desktop = sf::VideoMode::getDesktopMode();
+	sf::RenderWindow window(sf::VideoMode(640, 480, desktop.bitsPerPixel), "Lesson 8");
+
+
+	Image map_image;//объект изображения для карты
+	map_image.loadFromFile("map.png");//загружаем файл для карты
+	Texture map;//текстура карты
+	map.loadFromImage(map_image);//заряжаем текстуру картинкой
+	Sprite s_map;//создаём спрайт для карты
+	s_map.setTexture(map);//заливаем текстуру спрайтом
+
+
 
 	while (window.isOpen())
 	{
@@ -14,9 +25,22 @@ int main()
 			if (event.type == sf::Event::Closed)
 				window.close();
 		}
+		for (int i = 0; i < HEIGHT_MAP; i++)
+			for (int j = 0; j < WIDTH_MAP; j++)
+			{
+				if (TileMap[i][j] == ' ')  s_map.setTextureRect(IntRect(0, 0, 32, 32)); //если
+																						//встретили символ пробел, то рисуем 1-й квадратик
+				if (TileMap[i][j] == 's')  s_map.setTextureRect(IntRect(32, 0, 32, 32));//если
+																						//встретили символ s, то рисуем 2й квадратик
+				if ((TileMap[i][j] == '0')) s_map.setTextureRect(IntRect(64, 0, 32, 32));//если
+																						 //встретили символ 0, то рисуем 3й квадратик
 
-		window.clear();
-		window.draw(shape);
+				s_map.setPosition(j * 32, i * 32);//раскладываем квадратики в карту.
+
+				window.draw(s_map);//рисуем квадратики на экран
+			}
+
+	
 		window.display();
 	}
 
